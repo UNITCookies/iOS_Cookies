c6d7ced908745fb7baf442e63565421460699eb4
gabriel.jeong <gabriel.jeong@kakaomobility.com>
2022-09-03 10:47:20 +0900

    pod init

---
+++ Cookies/Pods/Target Support Files/Pods-Cookies-CookiesUITests/Pods-Cookies-CookiesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cookies_CookiesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Cookies_CookiesUITests
+@end
---
+++ Cookies/Pods/Target Support Files/Pods-CookiesTests/Pods-CookiesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CookiesTests : NSObject
+@end
+@implementation PodsDummy_Pods_CookiesTests
+@end
