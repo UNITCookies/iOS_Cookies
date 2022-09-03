1470c69d0a0df2ac7c3a79a1ab3d81da68dad64e
Kim HeeJae <iowa329@gmail.com>
2022-09-04 01:14:46 +0900

    [FIX] 폰트 및 색상 정리

---
+++ Cookies/Cookies/Common/Paragraph/UIColor+.swift
@@ -0,0 +1,33 @@
+//
+//  UIColor+.swift
+//  Cookies
+//
+//  Created by Kim HeeJae on 2022/09/03.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var main: UIColor {
+        return UIColor(red: 38.0/255.0, green: 38.0/255.0, blue: 38.0/255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var sub: UIColor {
+        return UIColor(red: 241.0/255.0, green: 232.0/255.0, blue: 217.0/255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var black: UIColor {
+        return UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray: UIColor {
+        return UIColor(red: 163.0/255.0, green: 163.0/255.0, blue: 163.0/255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var white: UIColor {
+        return UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+    }
+    
+}
+ 
